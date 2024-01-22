f4c5f1a5dafb18c82c1aa6b33782ef113d0458b6
Ryan Caltabiano <rcaltabiano90@gmail.com>
2024-01-22 15:49:03 -0500

    Working on router, did edits to navtabs, app.jsx and main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,42 @@
+import  ReactDOM  from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import App from './App';
+import ErrorPage from "./pages/error";
+// import Home from "./pages/Home";
+import Contact from "./pages/contact";
+import Portfolio from "./pages/portfolio";
+import About from "./pages/about";
+import Resume from "./pages/resume";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                index: true,
+                element: <About />,
+            },
+            {
+                path: '/contact',
+                element: <Contact />,
+            },
+            {
+                path: '/portfolio',
+                element: <Portfolio />
+            },
+            {
+                path: '/resume',
+                element: <Resume />
+            }
+        ]
+    }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router} />
+);
